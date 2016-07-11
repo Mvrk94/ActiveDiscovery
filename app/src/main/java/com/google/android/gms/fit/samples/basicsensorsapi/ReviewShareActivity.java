4335dcd41afb060e5fc4482210041e210179dd6b
chrisvandesande <chrisvdsande@gmail.com>
2016-07-11 21:22:47 +0200

    ReviewShare

--- app/src/main/java/com/google/android/gms/fit/samples/basicsensorsapi/ReviewShareActivity.java
+++ app/src/main/java/com/google/android/gms/fit/samples/basicsensorsapi/ReviewShareActivity.java
@@ -1,8 +1,12 @@
 package com.google.android.gms.fit.samples.basicsensorsapi;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +15,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
 import android.widget.RatingBar;
 import android.widget.RemoteViews;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.*;
 
@@ -24,6 +30,10 @@ import com.google.android.gms.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.ref.ReferenceQueue;
 
 public class ReviewShareActivity extends AppCompatActivity {
@@ -31,20 +41,47 @@ public class ReviewShareActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1313;
     ImageView imgtakenPhoto;
     TextView txtPoints;
+    TextView txtsharePoints1;
+   // TextView txtsharePoints2;
     Button btnSelfie;
     Button fbButton;
     RatingBar rbNumStars;
     Button btnAccept;
     Bitmap thumbnail;
+    EditText edtComment;
+    String path;
+    Uri tempUri;
+    File finalFile;
+    int Count=0;
     int points =0;
+    int sharePoints =0;
+    File dir;
+    String possiblePath;
+
 
     public Button btnGoogleShare;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review_rewards_share);
+        edtComment = (EditText) findViewById(R.id.editText);
+        edtComment.setFocusable(false);
 
-        txtPoints = (TextView) txtPoints.findViewById(R.id.pointsTextView);
+       // points = WorkoutActivity.discoveryPoints;
+        points =5;
+        sharePoints = 2;
+
+        dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        txtPoints = (TextView) findViewById(R.id.pointsTextView);
+        txtPoints.setText(String.valueOf(points));
+
+
+        txtsharePoints1 = (TextView) findViewById(R.id.sharePoints1);
+     //   txtsharePoints2 = (TextView) findViewById(R.id.sharePoints2);
+
+     //   txtsharePoints1.setText("(+"+sharePoints+" extra points)");
+     //   txtsharePoints2.setText("(+"+sharePoints+" extra points)");
 
         btnSelfie = (Button) findViewById(R.id.btnSelfie);
         btnSelfie.setEnabled(false);
@@ -59,7 +96,7 @@ public class ReviewShareActivity extends AppCompatActivity {
         btnGoogleShare = (Button) findViewById(R.id.share_button);
         btnGoogleShare.setOnClickListener(new btnGoogleShareClick());
         btnGoogleShare.setEnabled(false);
-
+     //   saveImage();
 
     //    fbButton = (Button) findViewById(R.id.shareButton);
 
@@ -95,15 +132,45 @@ public class ReviewShareActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode==REQUEST_IMAGE_CAPTURE){
-             thumbnail = (Bitmap) data.getExtras().get("data");
+            thumbnail = (Bitmap) data.getExtras().get("data");
             imgtakenPhoto.setImageBitmap(thumbnail);
 
+
+            tempUri = getImageUri(getApplicationContext(), thumbnail);
+
+            // CALL THIS METHOD TO GET THE ACTUAL PATH
+
+           finalFile = new File(getRealPathFromURI(tempUri));
+            //finalFile = new File(dir,"selfie.jpeg");
 
 
         }
 
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+     //   Toast.makeText(getApplicationContext(),path,Toast.LENGTH_SHORT).show();
+        if(Count==2) {
+            txtsharePoints1.setTextColor(getResources().getColor(R.color.dark_green));
+            txtsharePoints1.setText(sharePoints + "points added.");
+            int updatedpoints = points + sharePoints;
+            txtPoints.setText(String.valueOf(updatedpoints));
+        }
+        if(Count==1){
+          //  Toast.makeText(getApplicationContext(),getApplicationInfo().dataDir,Toast.LENGTH_LONG).show();
+            saveFile(getApplicationContext(), thumbnail, "Image");
+          //  Toast.makeText(getApplicationContext(),possiblePath,Toast.LENGTH_SHORT).show();
+
+          //  String path = saveToInternalStorage(thumbnail);
+           //   String path1 = obtainPath(getApplicationContext(),"image");
+           // finalFile = new File(path,"profile.jpg");
+            finalFile = new File(path,"Image.png");
+        }
+
+
+    }
 
     class btnSelfieClicker implements Button.OnClickListener{
 
@@ -114,7 +181,10 @@ public class ReviewShareActivity extends AppCompatActivity {
             startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
 
             btnGoogleShare.setEnabled(true);
-          //  fbButton.setEnabled(false);
+            Count+=1;
+
+          //  path = MediaStore.Images.Media.insertImage(getContentResolver(), thumbnail, "Image", null);
+
 
         }
     }
@@ -129,6 +199,7 @@ public class ReviewShareActivity extends AppCompatActivity {
             btnAccept.setEnabled(true);
 
             btnGoogleShare.setEnabled(true);
+            edtComment.setFocusable(true);
          //   fbButton.setEnabled(false);
         }
     }
@@ -139,36 +210,128 @@ public class ReviewShareActivity extends AppCompatActivity {
         public void onClick(View v) {
 
             String type = "image/*";
-            createInstagramIntent(type);
-
+            createShareIntent(type);
 
         }
     }
 
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
+    private void createShareIntent(String type){
+    //  Toast.makeText(getApplicationContext(),finalFile.getAbsolutePath(),Toast.LENGTH_SHORT).show();
 
-    private void createInstagramIntent(String type){
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_SEND);
 
         // Create the new Intent using the 'Send' action.
-        Intent share = new Intent(Intent.ACTION_SEND);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 
-        // Set the MIME type
-        share.setType("image/*");
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), thumbnail,"Title", null);
+        Uri imageUri = Uri.parse(path);
+        i.putExtra(Intent.EXTRA_STREAM, imageUri);
 
-        // Create the URI from the media
-        Uri imgUri = getImageUri(ReviewShareActivity.this,thumbnail);
+        i.putExtra(Intent.EXTRA_TEXT, "#WorkoutWithVitality");
+        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivity(Intent.createChooser(i,"ShareImage"));
+//        Intent i = new Intent(Intent.ACTION_SEND);
+//        i.setType("*/*");
+//        i.setAction(Intent.ACTION_SEND);
+//      //  i.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(finalFile));
+//       // i.putExtra(Intent.EXTRA_STREAM, Uri.parse(finalFile.getAbsolutePath()));
+//        i.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(finalFile));
+//        i.putExtra(Intent.EXTRA_TEXT, "#WorkoutWithVitality");
+//        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
 
-        // Add the URI to the Intent.
-        share.putExtra(Intent.EXTRA_STREAM, imgUri);
 
-        // Broadcast the Intent.
-        startActivity(Intent.createChooser(share, "Share to"));
+        startActivity(Intent.createChooser(i, "Share Image"));
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+       path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+            Toast.makeText(getApplicationContext(),path,Toast.LENGTH_SHORT).show();
+        return Uri.parse(path);
+    }
+//
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+
+    }
+
+
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+
+
+        File directory = cw.getDir("imageDir", Context.MODE_ENABLE_WRITE_AHEAD_LOGGING);
+        // Create imageDir
+        File mypath = new File(directory,"profile.png");
+
+
+
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //return directory.getAbsolutePath();
+        return directory.getAbsolutePath();
+    }
+
+
+    public void saveFile(Context context, Bitmap b, String picName){
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(picName, Context.MODE_PRIVATE);
+            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+        }
+        catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    public  String obtainPath(Context context,String picName)  {
+        String path = null;
+        FileInputStream fis;
+
+        try {
+            fis = context.openFileInput(picName);
+             path = context.getPackageResourcePath();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return path;
+
     }
 }
